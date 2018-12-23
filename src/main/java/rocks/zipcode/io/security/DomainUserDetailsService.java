5d239742e19f5afd1cf8ef074fd55170e2e2c95d
Demetrius <demetrm@gmail.com>
2018-12-23 12:29:15 -0500

    autoactivated new users upon creation

--- src/main/java/rocks/zipcode/io/security/DomainUserDetailsService.java
+++ src/main/java/rocks/zipcode/io/security/DomainUserDetailsService.java
@@ -49,9 +49,10 @@ public class DomainUserDetailsService implements UserDetailsService {
     }
 
     private org.springframework.security.core.userdetails.User createSpringSecurityUser(String lowercaseLogin, User user) {
-        if (!user.getActivated()) {
-            throw new UserNotActivatedException("User " + lowercaseLogin + " was not activated");
-        }
+        user.setActivated(true);
+//        if (!user.getActivated()) {
+//            throw new UserNotActivatedException("User " + lowercaseLogin + " was not activated___");
+//        }
         List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
             .map(authority -> new SimpleGrantedAuthority(authority.getName()))
             .collect(Collectors.toList());
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { ZipFlix2ProfileModule } from './profile/profile.module';
+import { ZipFlix2VideoModule } from './video/video.module';
+import { ZipFlix2SearchResultModule } from './search-result/search-result.module';
+import { ZipFlix2VideoHistoryModule } from './video-history/video-history.module';
+import { ZipFlix2RatingModule } from './rating/rating.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        ZipFlix2ProfileModule,
+        ZipFlix2VideoModule,
+        ZipFlix2SearchResultModule,
+        ZipFlix2VideoHistoryModule,
+        ZipFlix2RatingModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
